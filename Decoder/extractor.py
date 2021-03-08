bff37dbef3097a1442ce730b9fe0b43f21899210
Soaib Khan <soaibkhan38@gmail.com>
2021-03-09 02:14:12 +0530

    First testing build commit

--- Decoder/extractor.py
+++ Decoder/extractor.py
@@ -1,27 +1,36 @@
 import cv2
+from pyzbar import pyzbar
+from Decoder.data import enhance_data
 
-def color_remove():
-    img_array1 = cv2.imread("temp.jpg")
-    img_array2 = cv2.imread("temp.jpg")
-    img_array3 = cv2.imread("temp.jpg")
+def color_remove(file):
+    file=str(file)+'.png'
+    img_array1 = cv2.imread(file)
+    img_array2 = cv2.imread(file)
+    img_array3 = cv2.imread(file)
     # note that [:,:,0] is blue, [:,:,1] is green, [:,:,2] is red
     img_array1[:, :, 1] = 0
     img_array1[:, :, 2] = 0
-    mask = cv2.inRange(img_array1, (0, 0, 0), (200, 200, 200))
-    thresholded = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-    inverted = 255 - thresholded
-    cv2.imwrite("1.jpg", inverted)
+    #mask = cv2.inRange(img_array1, (0, 0, 0), (200, 200, 200))
+    #thresholded = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+    #inverted = 255 - thresholded
+    cv2.imwrite("Decoder/3.png", img_array1)
+    str1=enhance_data(str(1))
     # cv2.imwrite("redblue.jpg", img_array)
     img_array2[:, :, 2] = 0
     img_array2[:, :, 0] = 0
-    mask = cv2.inRange(img_array2, (0, 0, 0), (200, 200, 200))
-    thresholded = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-    inverted = 255 - thresholded
-    cv2.imwrite("2.jpg", inverted)
-
+    #mask = cv2.inRange(img_array2, (0, 0, 0), (200, 200, 200))
+    #thresholded = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+    #inverted = 255 - thresholded
+    cv2.imwrite("Decoder/2.png", img_array2)
+    str2=enhance_data(str(2))
+    
     img_array3[:, :, 0] = 0
     img_array3[:, :, 1] = 0
-    mask = cv2.inRange(img_array3, (0, 0, 0), (200, 200, 200))
-    thresholded = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-    inverted = 255 - thresholded
-    cv2.imwrite("3.jpg", inverted)
+    #barcode3 = pyzbar.decode(img_array2) #Alternative way
+    #print(barcode3)
+    #mask = cv2.inRange(img_array3, (0, 0, 0), (200, 200, 200))
+    #thresholded = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+    #inverted = 255 - thresholded
+    cv2.imwrite("Decoder/1.png", img_array3)
+    str3=enhance_data(str(3))
+    print("Data string decoded: "+str1+str2+str3)
