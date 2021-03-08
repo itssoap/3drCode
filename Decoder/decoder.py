e4f165a19c7619a84bb392dcf67bdc7c0fad7160
Soaib Khan <soaibkhan38@gmail.com>
2021-03-09 02:14:48 +0530

    First testing build commit

--- Decoder/decoder.py
+++ Decoder/decoder.py
@@ -1,8 +1,37 @@
-from PIL import Image
-from extractor import color_remove
-
-#tempholder=colorset("test.jpg")
-#woohoo_machine_learning("test.jpg")
-
-#print(colors)
-color_remove()
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+from PIL import Image, ImageFilter
+from pyzbar import pyzbar
+#%matplotlib inline
+def enhance_data(file):
+    image = cv2.imread(file+'.png') # reads the image
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) # convert to HSV
+    #figure_size = 9 # the dimension of the x and y axis of the kernal.
+    #new_image = cv2.medianBlur(image,figure_size) #, figure_size), 0)
+    #new_image1= cv2.cvtColor(new_image, cv2.COLOR_HSV2RGB)
+    new_image1= cv2.cvtColor(image, cv2.COLOR_HSV2RGB)
+    x=10
+    datum=[]
+    while not datum and x<=200:
+        i=3
+        tup=tuple([(x)]*3)
+        print(tup,)
+        mask = cv2.inRange(new_image1, (0, 0, 0), tup)
+        thresholded = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+        inverted = 255 - thresholded
+        datum.append(pyzbar.decode(inverted))
+        print(datum)
+        if not datum[0]:
+            datum=[]
+        #print("hehe")
+        x=x+5
+    return datum[0][0][0].decode()
+    plt.figure(figsize=(11,6))
+    plt.subplot(121), plt.imshow(cv2.cvtColor(image, cv2.COLOR_HSV2RGB)),plt.title('Original')
+    plt.xticks([]), plt.yticks([])
+    #plt.subplot(132), plt.imshow(cv2.cvtColor(new_image, cv2.COLOR_HSV2RGB)),plt.title('Median filter')
+    #plt.xticks([]), plt.yticks([])
+    plt.subplot(122), plt.imshow(cv2.cvtColor(inverted, cv2.COLOR_BGR2RGB)),plt.title('Thresholded filter')
+    plt.xticks([]), plt.yticks([])
+    plt.show()
