a9bdff0817551ee75dad491013a47d492cb11448
Soaib Khan <soaibkhan38@gmail.com>
2021-03-09 02:12:52 +0530

    First testing build commit

--- Decoder/scanner.py
+++ Decoder/scanner.py
@@ -3,29 +3,37 @@ from pyzbar import pyzbar
 import numpy as np
 from PIL import Image
 from time import sleep
+import random
+from Decoder.extractor import color_remove
 
-def main():
+def ipcam():
     #1
     camera = cv2.VideoCapture(0)
+    choice=input("Do you want to use:\n 1. Webcam\n2. IPCam\nEnter your choice: ")
+    if choice==2:
+        address= input("Enter IPCam address: ") #using IP cam 
+        camera.open(address)
+    else:
+        pass    
     ret, frame = camera.read()
     #2
     
     while ret:
-        sleep(.5)
+        #sleep(.1)
         ret, frame = camera.read()
         barcode_info=None
         pts=np.zeros((4,1,2))
         barcodes = pyzbar.decode(frame)
         for barcode in barcodes:
-             x, y , w, h = barcode.rect
+            x, y , w, h = barcode.rect
         #1
             
-             barcode_info = barcode.data.decode('utf-8')
-             pts = np.array([barcode.polygon], np.int32)
-             pts = pts.reshape(-1,1,2)
+            barcode_info = barcode.data.decode('utf-8')
+            pts = np.array([barcode.polygon], np.int32)
+            pts = pts.reshape(-1,1,2)
         #cropped = frame[pts]
         #cv2.imwrite('cropped.jpg', cropped)
-             cv2.polylines(frame, [pts], True, (0, 255, 0), 1)
+            cv2.polylines(frame, [pts], True, (0, 255, 0), 1)
         x1=pts[0][0][0]
         y1=pts[0][0][1]
         x2=pts[1][0][0]
@@ -41,13 +49,14 @@ def main():
         bot_right_y = max([y1,y2,y3,y4])
         cv2.imshow('Barcode/QR code reader', frame)
         if cv2.waitKey(1) & 0xFF == 27 or barcode_info!=None:
-             cv2.imwrite('Capture.png', frame[top_left_y:bot_right_y+1, top_left_x:bot_right_x+1])
-             print(pts)
-             break
+            name=''.join(random.choices(string.ascii_uppercase +
+                             string.digits, k = 5))
+            cv2.imwrite(str(name)+'.png', frame[int(top_left_y):int(bot_right_y+1), int(top_left_x):int(bot_right_x+1)])
+            print(pts)
+            color_remove(name)
+            break
         
     #3
     camera.release()
     cv2.destroyAllWindows()
 #4
-if __name__ == '__main__':
-    main()
